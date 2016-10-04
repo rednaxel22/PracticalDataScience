e5605191ae6812adead1a24e8677696e946900c2
rednaxel22 <michel.alex.castillo@gmail.com>
2016-10-04 15:03:42 -0500

    Initial commit

---
+++ test1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  4 09:42:18 2016
+
+@author: Mihcael Castillo
+"""
+
+import pandas as pd
+
+data = pd.read_excel('report.xlsx',sheetname='Series-layout A', skiprows=1, header=0)
+
+len(data)               # Get Dataframe rows len
+data.iloc[:,0:3]        # Get all rows and firts 3 columns
+len(data.columns)       # Get the columns len
+
+data.iloc[:,0].unique() # Distinct Countries
+data.iloc[:,1].unique() # Distinct Years
+
+min(data.iloc[:,1])     # Min Year
+max(data.iloc[:,1])     # Max Year
+
+dataUS = data[data.iloc[:,0] == 'United States']    # Filter by Index
+dataUS = dataUS.iloc[:,0:3]                         # Select the first 3 columns
+dataUS
+
+data[data.loc[:,'Country'] == 'United States']      # Filter by ColumnName
